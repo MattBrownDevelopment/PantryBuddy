5e76f494a6ad312949cf992cda9e50ec806bcbab
MatthewBrown <mattwesbrown@gmail.com>
2017-03-03 15:51:08 -0500

    Food class might be working, user input list sort of working.

---
+++ app/src/androidTest/java/Pantry.java
@@ -0,0 +1,11 @@
+import com.example.matthewbrown.testapp.FoodItem;
+import java.util.*;
+/**
+ * Created by MatthewBrown on 3/3/2017.
+ */
+
+public class Pantry {
+    ArrayList<FoodItem> storage = new ArrayList<FoodItem>();
+
+
+}
---
+++ app/src/main/java/com/example/matthewbrown/testapp/FoodItem.java
@@ -0,0 +1,87 @@
+package com.example.matthewbrown.testapp;
+
+public class FoodItem {
+
+    private String itemName;
+    private double daysLeft;
+    private double daysSincePurchased;
+    private double usagePerDay;
+    private double amount;
+    private double oldAmount;
+    private double oldDays;
+    private boolean alreadyBought = false;
+    private boolean trendFormed = false;
+
+    public FoodItem()
+    {
+
+    }
+
+    public FoodItem(String n, double qty)
+    {
+        itemName = n;
+        setAmount(qty);
+    }
+
+    public void buy(double qty)
+    {
+        oldAmount = amount; //Keep track of previous amount before buying more
+        setAmount(getAmount() + qty);
+        oldDays = daysSincePurchased;
+        if(alreadyBought)
+        {
+            usagePerDay = (amount - oldAmount) / ( oldDays );
+            trendFormed = true;
+        }
+        daysSincePurchased = 0;
+    }
+
+    public double getDaysLeft() {
+        daysLeft = amount / usagePerDay;
+        return daysLeft;
+    }
+    public void setDaysLeft(double daysLeft) {
+        this.daysLeft = daysLeft;
+    }
+    public String getItemName() {
+        return itemName;
+    }
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    public double getDaysSincePurchased() {
+        return daysSincePurchased;
+    }
+    public void setDaysSincePurchased(double d) {
+        daysSincePurchased = d;
+        if(trendFormed)
+        {
+            amount -= daysSincePurchased * usagePerDay;
+        }
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double a) {
+        amount = a;
+    }
+
+
+    public boolean isAlreadyBought() {
+        return alreadyBought;
+    }
+
+    public void setAlreadyBought(boolean alreadyBought) {
+        this.alreadyBought = alreadyBought;
+    }
+
+    public double getUsagePerDay()
+    {
+        return usagePerDay;
+    }
+
+
+
+}
