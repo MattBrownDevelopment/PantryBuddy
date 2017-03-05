ae401917a0bc1c20cd09ea5cca48935b9082c431
MatthewBrown <mattwesbrown@gmail.com>
2017-03-05 00:24:11 -0500

    Trying to push to my own repo. Past 6 hours of commits have been going nowhere.

--- app/src/main/java/com/example/matthewbrown/testapp/DatabaseHandler.java
+++ app/src/main/java/com/example/matthewbrown/testapp/DatabaseHandler.java
@@ -73,7 +73,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     {
     int x = 5;
     System.out.println(x);
-        System.out.println("hello");
+    System.out.println("hello");
     }
 
 }
